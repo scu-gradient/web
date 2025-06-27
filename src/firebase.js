08321020974592932d781db5aae09ce0b3d1e282
rshirams_ <127058258+rshiramss@users.noreply.github.com>
2025-06-27 16:42:09 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"; // if you need auth
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD1UAYMkJrOzz_PZoXvVkxP4mfiIuffys0",
-  authDomain: "vector-db-gradient.firebaseapp.com",
-  projectId: "vector-db-gradient",
-  storageBucket: "vector-db-gradient.firebasestorage.app",
-  messagingSenderId: "543029355000",
-  appId: "1:543029355000:web:09486379a739622f9c760d"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
